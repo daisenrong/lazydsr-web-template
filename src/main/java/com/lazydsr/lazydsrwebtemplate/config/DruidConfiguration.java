95fe792dfe79973be1af06ed7b56293f10e0bfbf
lazydsr <168367406@qq.com>
2018-03-05 17:58:27 +0800

    Init: 使用的SpringBoot2.0 druid为1.1.8（后期进行一下升级），数据库用的mysql5.6

--- src/main/java/com/lazydsr/lazydsrwebtemplate/config/DruidConfiguration.java
+++ src/main/java/com/lazydsr/lazydsrwebtemplate/config/DruidConfiguration.java
@@ -2,8 +2,12 @@ package com.lazydsr.lazydsrwebtemplate.config;
 
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -23,12 +27,12 @@ import javax.sql.DataSource;
 @Configuration
 @Slf4j
 public class DruidConfiguration {
-    @Primary
-    @Bean
-    @ConfigurationProperties("spring.datasource.druid")
-    public DataSource dataSourceOne(){
-        return DruidDataSourceBuilder.create().build();
-    }
+    //@Primary
+    //@Bean
+    //@ConfigurationProperties("spring.datasource.druid")
+    //public DataSource dataSourceOne(){
+    //    return DruidDataSourceBuilder.create().build();
+    //}
     //@Bean
     //@ConfigurationProperties("spring.datasource.druid.two")
     //public DataSource dataSourceTwo(){
@@ -56,35 +60,35 @@ public class DruidConfiguration {
     //public ServletRegistrationBean servletRegistrationBean(){
     //    return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
     //}
-    //@Bean(name = "dataSource")
-    //@Primary
-    //@ConfigurationProperties(prefix = "spring.datasource.druid")
-    //public DataSource dataSource() {
-    //    return DataSourceBuilder.create().type(DruidDataSource.class).build();
-    //}
-    //
-    //@Bean
-    //public ServletRegistrationBean statViewServlet() {
-    //    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-    //    //白名单：
-    //    servletRegistrationBean.addInitParameter("allow", "172.25.1.233,172.25.80.36,127.0.0.1");
-    //    //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的即提示:Sorry, you are not permitted to view this page.
-    //    servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
-    //    //登录查看信息的账号密码.
-    //    servletRegistrationBean.addInitParameter("loginUsername", "druid");
-    //    servletRegistrationBean.addInitParameter("loginPassword", "12345678");
-    //    //是否能够重置数据.
-    //    servletRegistrationBean.addInitParameter("resetEnable", "false");
-    //    return servletRegistrationBean;
-    //}
-    //
-    //@Bean
-    //public FilterRegistrationBean statFilter() {
-    //    FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-    //    //添加过滤规则.
-    //    filterRegistrationBean.addUrlPatterns("/*");
-    //    //添加不需要忽略的格式信息.
-    //    filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-    //    return filterRegistrationBean;
-    //}
+    @Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource dataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow", "172.25.1.233,172.25.80.36,127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的即提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername", "druid");
+        servletRegistrationBean.addInitParameter("loginPassword", "12345678");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean statFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
 }
